2a4d6015fdc3b597ec87fe4661b42b57aac5496a
Aravind <gmarav005@gmail.com>
2026-02-04 12:39:27 +0530

    Add Proxy file Middleware

---
+++ src/proxy.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+
+export function proxy(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const isPublicPath = path === '/login' || path === '/signup'
+
+    const token = request.cookies.get('token')?.value || ''
+
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.url))    
+    }
+
+  return NextResponse.redirect(new URL('/home', request.url))
+}
+ 
+ 
+export const config = {
+  matcher: [
+    "/",
+    "/profile",
+    '/login',
+    '/signup',
+  ]
+}
